4fa239bbdc7b5c9504a5b8e59e79693d20e34180
angelsancheztf <angelsancheztrillo@gmail.com>
2022-01-11 19:22:24 +0100

    cambios

--- TEMA6.1/getCities.php
+++ TEMA6.1/getCities.php
@@ -1,15 +1,47 @@
-<?php if($datos_paises->num_rows > 0){ ?>
+<?php
+
+$servername = "localhost";
+$database = "world";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+mysqli_select_db($conn, "world");
+
+if (isset($_GET["button"])) {
+
+    $consulta_ciudad = "SELECT `Name` FROM `city` ORDER BY Population DESC LIMIT 10 ";
+
+    $datos_ciudad = mysqli_query($conn, $consulta_ciudad);
+
+
+    /*if($datos_ciudad->num_rows > 0){
+        foreach ($datos_ciudad as $clave => $valor){
+            echo"<h3>".$valor['Name']."</h3>";
+        } 
+    }
+    */
+
+    if ($datos_ciudad->num_rows > 0) { ?>
         <table border="2">
             <tr>
-                <th>CIUDADES</th>
-                <th><?php echo "<h4>Ciudades =>".$datos_paises->num_rows."</h4>"; ?></th>
-            </tr>
-            <?php foreach ($datos_paises as $clave => $valor){ ?>
-            <tr>
+                <th>Ciudades</th>
 
-                <td> <?php echo $valor['Name']; ?> </td>
-                              
             </tr>
+            <?php foreach ($datos_ciudad as $clave => $valor) { ?>
+                <tr>
+                    <td> <?php echo $valor['Name']; ?> </td>
+
+                </tr>
             <?php } ?>
         </table>
-<?php } ?>
+    <?php } ?>
+<?php
+}
+?>
--- TEMA6.1/index.php
+++ TEMA6.1/index.php
@@ -3,15 +3,15 @@
     $database = "world";
     $username = "root";
     $password = "";
-            
+
     $conn = mysqli_connect($servername, $username, $password, $database);
 
-    if(!$conn) {
-        die("Connection failed: ".mysqli_connect_error());
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
     }
-    
-    $consulta_paises_1="SELECT `Code`, `Name` FROM `country`";
-    
+
+    $consulta_paises_1 = "SELECT `Code`, `Name` FROM `country`";
+
     mysqli_select_db($conn, "world");
 
     $datos = mysqli_query($conn, $consulta_paises_1);
@@ -25,32 +25,89 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="scripts.js"></script>
+    <!--<script src="scripts.js"></script>-->
 </head>
 <body>
     <!--Solo ciudades-->
 
-    <form action="getCities.php" method='get'>
+    <form action="bbdd.php" method='get'>
         <select name='pais'>
             <?php
-                if($datos->num_rows > 0){
-                    foreach ($datos as $clave => $valor){
-                        echo"<option value='".$valor['Code']."'>".$valor['Name']."</option>";
-                    }
+            if ($datos->num_rows > 0) {
+                foreach ($datos as $clave => $valor) {
+                    echo "<option value='" . $valor['Code'] . "'>" . $valor['Name'] . "</option>";
                 }
+            }
             ?>
         </select>
-        
+
         <br><br>
         <input type='submit' value='Peticion HTML'>
         <br><br>
     </form>
 
-    <form action="world2.php" method="get">
-        <br><br>
-        <input type="submit" value="Peticion AJAX XMLHTTPREQUEST " name="button" onclick=" location.href='recogeCiudades(getCities.php)' ">
-        <br><br>
-        <input type="submit" value="Peticion AJAX JQUERY" name="button" onclick=" location.href='recogeCiudades()' ">
-    </form>
+    <!--<form action="getCities.php" method="get">-->
+    <br><br>
+    <!--<input type="submit" value="Peticion AJAX XMLHTTPREQUEST " name="button" onclick=" recogeCiudades('getCities.php') ">-->
+    <button onclick="botonXML()">XML</button>
+    <br><br>
+    <!--<input type="submit" value="Peticion AJAX JQUERY" name="button" id="ho" onclick="botonQuery()">-->
+    <button onclick="botonQuery()">JQuery</button>
+    <!--</form>-->
+    <script>
+    
+
+        function botonQuery() {
+            let req = new XMLHttpRequest();
+            var pais = document.getElementById("pais").value;
+            console.log(pais);
+
+            req.open("GET", 'http://localhost/dwes21/TEMA6.1/bbdd.php', true);
+            req.onreadystatechange = () => {
+                if(req.readyState == 4){
+                    document.getElementById("hola").innerHTML = req.responseText;
+                }
+            }
+            var data = new FormData();
+            data.append('country', pais);
+
+            req.send(data);
+        }
+
+        function botonXML(){
+            if (window.XMLHttpRequest) {
+                var http_request = new XMLHttpRequest();
+            } else if (window.ActiveXObject) {
+                var http_request = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+
+            http_request.open(
+                'GET',
+                url,
+                true
+            );
+
+            http_request.send();
+
+            if (http_request.readyState == 4 && http_request.status == 200) {
+                alert("todo va bien");
+            } else {
+                alert("algo falla");
+            }
+
+
+            $.ajax({
+                type: "POST",
+                url: 'getCities.php',
+                data: {
+                    selectCountry: country.value
+                },
+                success: function(response) {
+                    var element = document.getElementById("");
+                    element.innerHTML = response;
+                }
+            });
+        }
+    </script>
 </body>
 </html>
